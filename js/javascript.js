ef4567e295a305fa571a7f4850ce33dc465fa475
navin-kumar-srnk <naveenkumarsk31@gmail.com>
2020-04-26 11:35:17 +0530

    test

---
+++ js/javascript.js
@@ -0,0 +1,26 @@
+
+window.addEventListener("load", ()=>{
+   
+   if(navigator.geolocation)
+   {
+       navigator.geolocation.getCurrentPosition(pos=>{
+        navigator.geolocation.getCurrentPosition(position=>{
+            lat=position.coords.latitude;
+            lon=position.coords.longitude;
+            const url='http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid=c4fca9daeed78b9f45e1f6cfeb035582&units=metric';
+            fetch(url).then(data=>{
+                return data.json().then(d=>{
+                    console.log(d);
+                    document.getElementById('name').innerText=d.name;
+                    document.getElementById('temp').innerText=d.main.temp+"";
+                    document.getElementById('weather').innerText=d.weather[0].main+"";
+                })
+            })
+        })
+           
+       })
+   }
+               
+});
+
+
